d5281b4d00ae4763c0e72e5a2aa8dffd9785b8eb
SravaniSingam-WAL <sravani.s@westagilelabs.com>
2024-01-16 17:36:21 +0530

    added basic css

--- src/home.js
+++ src/home.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { getBrandName } from "./utils";
 
 const Home = () => {
-  
+  const [brandName, setBrandName]=useState(getBrandName())
+ 
   return (
     <div>
       <h2>Home Page for Clas Application</h2>
+      <h4>BrandName : {brandName}</h4>
     </div>
   );
 };
--- src/login.js
+++ src/login.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 //import { useAsync } from "react-async";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-//import { setToken } from "./utils";
+import { setToken } from "./utils";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error,setError] = useState("")
   const navigate = useNavigate();
 
-  const handleLoginOne = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     console.log("Username:", username);
     console.log("Password:", password);
-    const tenantId = username === "dhruv@gmail.com" ? 2 : 1;
-    console.log(tenantId, "---------------------");
+    try{
     const result = await axios.post(
       "http://localhost:3030/api/login",
       {
@@ -22,27 +22,31 @@ const Login = () => {
       },
       {
         headers: {
-          "Content-Type": "application/json",
-          TenantId: tenantId,
+          "Content-Type": "application/json"
         },
       }
     );
     if (result) {
       console.log("Result", result);
       console.log("-=======", result.data.user);
-    //  setToken(result.data.user);
+      setToken(result.data.user);
       navigate("/home");
     }
+  }  catch(error){
+    if(error.response && error.response.status === 401){
+      console.log('Invalid email or password')
+      setError('Invalid email or password')
+    }
+    else{
+    console.log(error.message,'response')
+    setError(error.message)
+    }
+  }
   };
 
-  const handleLogin = ()=>{
-    console.log('Login is Done')
-    navigate("/home");
-    
-  }
   return (
     <div>
-      <h2>Login Page</h2>
+      <h2 className="loginText">Login Page</h2>
       <form onSubmit={handleLogin}>
         <label>
           Username:
@@ -64,6 +68,7 @@ const Login = () => {
         <br />
         <button type="submit">Login</button>
       </form>
+      {error && <p className="error-message">{error}</p>}
     </div>
   );
 };
