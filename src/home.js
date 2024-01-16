4083c84704b9f09e66dc9871b057169332e50d8e
SravaniSingam-WAL <sravani.s@westagilelabs.com>
2024-01-16 09:51:36 +0530

    login changes

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  
+  return (
+    <div>
+      <h2>Home Page for Clas Application</h2>
+    </div>
+  );
+};
+
+export default Home;
+
