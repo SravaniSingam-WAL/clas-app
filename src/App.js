f3d0f4c4b7d14e1e6e4ed6f5a7c8a01db189ad84
SravaniSingam-WAL <sravani.s@westagilelabs.com>
2024-01-29 14:35:20 +0530

    added base path

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login';
 import Home from './home';
-
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory({ basename: '/clas' });
 function App() {
   return (
-    <Router>
+    <Router history={history} basename="/clas">
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
